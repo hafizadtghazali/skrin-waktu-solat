313ca7b93905bba97a35de67eb2fa84c5b5bd720
hafizadt ghazali <hafizadt.ghazali@gmail.com>
2024-04-30 19:20:05 +0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,7 @@ const config: Config = {
   theme: {
     extend: {
       backgroundImage: {
+        "gradient-bg": "linear-gradient(to top, #a8edea 0%, #fed6e3 100%)",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
